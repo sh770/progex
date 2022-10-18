fdfa0d1d0e1abbbbbe5434f8ff1dcad167299f60
sh <sh770h@gmail.com>
2022-10-18 16:37:40 +0300

    שם

---
+++ test/ts1/local.js
@@ -0,0 +1,43 @@
+window.onload = function(){
+    checkLocal();
+    declareViewEvents();
+    clir();
+    }
+
+    function checkLocal(){
+    if(localStorage["user"]){
+        document.querySelector("#id_h1").innerHTML = `
+        היי ${localStorage["user"]}<br>
+        ברוך הבא
+        <br>לאתר שלי
+        `
+        }
+        else {
+            document.querySelector("#id_h1").innerHTML = `
+              היי אורח<br>
+        ברוך הבא
+        <br>לאתר שלי
+        `
+          }
+    }
+
+    function declareViewEvents(){
+    let save_btn = document.querySelector("#save_btn");
+    save_btn.addEventListener("click", function(){
+    let input_val = document.querySelector("#id_input").value;
+    // alert(input_val)
+    localStorage.setItem("user", input_val);
+    checkLocal();
+    })
+}
+
+function clir(){
+    let save_btn2 = document.querySelector("#save_btn2");
+    
+    save_btn2.addEventListener("click", function(){
+       let input_val = ""; 
+       localStorage.clear();
+       checkLocal();
+    
+})
+}
