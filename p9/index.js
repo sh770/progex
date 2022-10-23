e5c19541eea6ffdb6b6a0b4aba9feba2c1ea25ed
sh <sh770h@gmail.com>
2022-10-23 20:49:23 +0300

    9

---
+++ p9/index.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+let x = 8;
+let y = 5;
+let z = x * y;
+console.log(z);
+
+let movieName = "sarHatabaot " ;
+let movieRating = " 5" ;
+let moveDirector = " spilberg" ;
+let resolt = movieName + movieRating + moveDirector;
+console.log(resolt);
