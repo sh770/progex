71055c479b0ab49bc054f4e740c70321a9f0bb77
sh <sh770h@gmail.com>
2022-10-23 21:45:06 +0300

    Create t9.1.js

---
+++ p9/t9.1.js
@@ -0,0 +1,30 @@
+let length = 50;
+let lastname = "jone";
+
+const person = {
+firstName:"avy",
+lastName:"cohen",
+} ;
+
+console.log(typeof(person));
+console.log(typeof(length));
+console.log(typeof(lastname));
+
+let x =10 ** 5;
+console.log(x);
+
+let z = 8 + 26 * 3 * 0 + 5;
+console.log(z);
+
+let t = 15 - 20;
+console.log(t);
+
+let u = 100 / 20;
+console.log(u);
+
+let ww = 8;
+let zz = 4+4;
+let wwr = zz ==zz;
+console.log(wwr);
+
+
