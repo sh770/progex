df710f0174e3c4ad48ddfffd1405a7af91c8389e
sh <sh770h@gmail.com>
2022-10-27 21:45:29 +0300

    10

---
+++ p10/1.js
@@ -0,0 +1,33 @@
+function myfun1(x) {
+    return (x * 53);
+}
+console.log(myfun1(2));
+
+function myfun2(x,y) {
+    return (x**y);
+}
+console.log(myfun2(2,3));
+
+function myfun3() {
+   let a = prompt("enter data a");
+   let b = prompt("enter data b");
+   let c = prompt("enter data c");
+   let d = prompt("enter data d");
+   let e = prompt("enter data e");
+   alert(`${a}\n${b}\n${c}\n${d}\n${e}`);
+}
+
+myfun3();
+
+function myfun4() {
+    let fName = prompt("enter data first Name");
+    let lName = prompt("enter data last Name");
+    let age = prompt("enter data age");
+
+    alert(`Your First Name is: ${fName}
+    \nYour Last Name Is: ${lName}
+    \nYou Are: ${age} years old`);
+ }
+ 
+ myfun4();
+ 
