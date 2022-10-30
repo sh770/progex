a3f5e3fb62b91bd62f61e3586baba149b73b3f6b
sh <sh770h@gmail.com>
2022-10-30 21:40:58 +0200

    11

---
+++ p11/script.js
@@ -0,0 +1,10 @@
+const aryy1 = ["a","b","e","d","e","f","g","h"];
+console.log(aryy1[3]);
+console.log(aryy1.length);
+console.log(aryy1[7]);
+aryy1[2] = "shalom";
+aryy1.push("harpaz");
+console.log(aryy1);
+
+const date = new Date();
+console.log(date);
