f972b89a1d755c5e43bfb4725dcaefd31b4e2b53
sh <sh770h@gmail.com>
2022-11-04 01:56:09 +0200

    12

---
+++ p12/p12.2.js
@@ -0,0 +1,15 @@
+// 7
+
+let x  = 10;
+let y  = 15;
+function myfx(max,min) {
+    let x4 = Math.floor(Math.random() * (max - min + 1)) + min;
+    return console.log(x4);
+    }
+myfx(x,y);
+
+// 8
+
+for (let i = 0 ; i < 10 ; i++){
+    console.log(" i = " + i);
+}
