00b0dc0a2a7862a9cb2c654f17bc901ad7f307dc
sh <sh770h@gmail.com>
2022-11-04 01:31:02 +0200

    uploud

--- p12/script.js
+++ p12/script.js
@@ -7,12 +7,33 @@ let x3 = (x1 < x2);
  
 
  // 2
-console.log(x1);
-Math.floor(x1);
-console.log(x1);
+
+let x4 =  Math.round(x1);
+
+console.log(x4);
+
+console.log(Math.round(x1));
 
  // 3
 
+let x5 = 15 > 10;
+console.log(x5);
+
  // 4
 
+ let x6 = 50 == 50;
+ console.log(x6);
+
  // 5
+
+ let x7 = -8 < 13;
+ console.log(x7);
+
+ // 6
+let x = 29;
+function myfun() {
+    
+  let xx =  (x == 30) ;
+    console.log(xx);
+}
+myfun(x);
