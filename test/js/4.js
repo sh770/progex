2533f1df0ffae63f63f3a1c7bb79facb0ecf3327
sh <sh770h@gmail.com>
2022-11-07 14:24:07 +0200

    //פיבונצי

--- test/js/4.js
+++ test/js/4.js
@@ -1,21 +1,32 @@
-
-let strUp ="";
-let strDn ="";
-function printStar(numSatr){
-for(let i=1 ; i<numSatr ; i++){
-     strUp ="";
-     strDn ="";
-
-        for (j=1 ; j<=i ;j++){
-               if(j - 1){
-                strUp = strUp + j + " ";
-                strDn = j - 1 + " " + strDn;
-               }
-               else{
-                strUp = j + " " + strUp;
-               }
-    }                                             
-    console.log("  ".repeat(numSatr - i)  + strUp + strDn );
- }
+// הגדרת משתנים
+let strUp = "";
+let strDn = "";
+//console.log(typeof(strUp));
+// הגדרת פונקציה
+function printStar(numSatr) {
+   //הגדרת לולאה 
+   //בתוך הלולאה מגדירים משתנה בשם i 
+   //   קובעים ש אם numSatr קטן מ i
+   // נוסיף אחד ל i
+   for (let i = 1; i < numSatr; i++) {
+      // קוראים שוב למשתנים שהגדרנו למעלה
+      strUp = "";
+      strDn = "";
+// נקבע משתנה בשם j ונריץ לולאה
+// ניתן לו את הערך 1 ואם הוא שווה או קטן מ i נוסיף לו אחד
+      for (let j = 1; j <= i; j++) {
+         // אם ניתן להפחית אחד
+         // strUp אז המשתנה 
+         // 
+         if (j - 1) {
+            strUp = strUp + j.toString().padStart(2, "0") + " ";
+            strDn = (j - 1).toString().padStart(2, "0") + " " + strDn;
+         }
+         else {
+            strUp = j.toString().padStart(2, "0") + " " + strUp;
+         }
+      }
+      console.log("   ".repeat(numSatr - i) + strUp + strDn);
+   }
 }
-printStar(10);
+printStar(31);
---
+++ test/js/5.js
@@ -0,0 +1,16 @@
+//חישוב סידרת מספרים פיבונאצית
+let n1=0;
+let n2=1;
+let n3=1;
+let fibo=[0,1];
+
+function fibonachi(num){
+  for(let i = 2; i < num ;i++){         
+    fibo[i] = n3;      
+    n1 = n2;       
+    n2 = n3;       
+    n3 = n1 + n2;   
+  }
+  return fibo;
+ }  
+ console.log(fibonachi(20)); 
