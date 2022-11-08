30710f9474a64f6f49aef0c89c363832563be6a2
sh <sh770h@gmail.com>
2022-11-08 23:23:57 +0200

    תרגיל 24 א

---
+++ p13/script.js
@@ -0,0 +1,103 @@
+function name() {
+let date = new Date;
+let answer = date.getDate();
+console.log(date.getDate())
+   switch (Number(answer)) {
+        case 1:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 2:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 3:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 4:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 5:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 6:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 7:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 8:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 9:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 10:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 11:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 12:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 13:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 14:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 15:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 16:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 17:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 18:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 19:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 20:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 21:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 22:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 23:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 24:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 25:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 26:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 27:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 28:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 29:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 30:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        case 31:
+            alert(`היום בחודש  הוא : ${answer}`);
+            break;
+        default:
+            alert("תקלה");
+            break;
+    }
+}
