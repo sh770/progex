3274f0c04f90876fce2437b94cb99a1411abc9f9
sh <sh770h@gmail.com>
2022-11-09 02:31:11 +0200

    תרגיל 23

---
+++ p13/p13.23.js
@@ -0,0 +1,20 @@
+// תרגיל 23
+
+let a = 18;
+
+
+if (a >= 95) {
+    console.log("Most Valuable Grandpa !");
+} else if (a >= 60) {
+    console.log("Please, call me sir");
+} else if (a < 18) {
+    console.log("Not valid");
+} else if (a == 50) {
+    console.log("GOLDEN");
+} else if (a >= 30) {
+    console.log("Aging like a fine wine aren\’t you?");
+} else if (a >= 18) {
+    console.log("You are still young but good to go");
+} else {
+    console.log("Not valid");
+}
