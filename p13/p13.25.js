5c2b5ed2636e62caf4dc2e11101b1ce4dfc4d305
sh <sh770h@gmail.com>
2022-11-09 23:57:19 +0200

    25 א,ב,ג,ג,ה,ו

--- p13/p13.25.js
+++ p13/p13.25.js
@@ -1,1 +1,31 @@
 // תרגיל 25
+
+// a
+let t = "aba mam sun doter";
+console.log(t.length);
+
+
+// b + c
+let r = ["Fruits", "Meat", "Vegetables"];
+console.log(r.slice(2));
+r.shift();
+r.shift();
+console.log(r);
+
+// d
+let e = "VHGCXEREYXYTYCR VHCHCYCRC";
+console.log("e: "+ e.toLowerCase());
+
+
+// E
+
+let q = "ftyyrdrterytyvrdeyc vhcyrdtrd";
+//console.log(q)
+console.log("q: "+q.toUpperCase());
+
+
+// f 
+let ww = "Fruits Meat Vegetables";
+
+console.log(ww.replace("Meat", "tree"));
+
