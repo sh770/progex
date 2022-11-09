20bf25405086c596abd085f9f1a16d755e6f9948
sh <sh770h@gmail.com>
2022-11-10 00:22:54 +0200

    היפוך

---
+++ test/js/7.js
@@ -0,0 +1,11 @@
+//היפוך סטרינג
+
+function revert(str) {
+    let tmp ="";
+    for (let i = 0 ; i < str.length ; i++) {
+        tmp = str[i] + tmp;
+        }
+    return tmp;
+}
+
+console.log(revert("wertyghjk"))
