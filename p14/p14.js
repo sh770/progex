685d4ffc7678cbb08468f806800ca0cfba5458e0
sh <sh770h@gmail.com>
2022-11-11 00:52:31 +0200

    26

---
+++ p14/p14.js
@@ -0,0 +1,29 @@
+// 1
+
+let xe = 18;
+let xx = xe.toString();
+
+console.log(typeof (xx));
+
+// 2
+
+let te = 458.95;
+
+let tew = te.toFixed();
+console.log(tew);
+
+// 3
+
+
+/////////////
+let x = 63.9;
+
+if (typeof (x) == (typeof (1))) {
+    console.log("num")
+    let rrq = x.toString();
+    console.log(rrq);
+    console.log(x.toFixed());
+    console.log((typeof (rrq)));
+} else {
+    console.log("no num")
+}
