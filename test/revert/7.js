1eb5f9a0a787d61fc76aca0fd9aeb29cf6b27fd4
sh <sh770h@gmail.com>
2022-11-10 15:32:33 +0200

    היפוך טקסט

---
+++ test/revert/7.js
@@ -0,0 +1,16 @@
+
+        String.prototype.reverse=function(){
+            return this.split("").reverse().join("");
+        }
+
+//היפוך סטרינג
+
+// function revert(str) {
+//     let tmp ="";
+//     for (let i = 0 ; i < str.length ; i++) {
+//         tmp = str[i] + tmp;
+//         }
+//     return tmp;
+// }
+
+// console.log(revert("wertyghjk"))
