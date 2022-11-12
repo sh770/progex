9ce413f896a5e11d26fea5fb7e07d82547319954
sh <sh770h@gmail.com>
2022-11-13 00:43:33 +0200

    27.שיפורים

--- p14/script.js
+++ p14/script.js
@@ -1,6 +1,39 @@
 // הצג ברוך הבא
 function fil() {
-    document.getElementById("h1").innerHTML = "ברוך הבא";
+    
+    let day = new Date().getDay()
+    let day2;
+switch(day){
+    case 0:
+        day2 = "ראשון"
+        console.log("Sunday")
+        break;
+    case 1:
+        day2 = "שני"
+        console.log("Monday")
+        break;
+    case 2:
+        day2 = "שלישי"
+        console.log("Tuesday")
+        break;
+    case 3:
+        day2 = "רביעי"
+        console.log("Wednesday")
+        break;
+    case 4:
+        day2 = "חמישי"
+        console.log("Thursday")
+        break;
+    case 5:
+        day2 = "שישי"
+        console.log("Friday")
+        break;
+    case 6:
+        day2 = "שבת"
+        console.log("Saturday")
+        break;   
+}
+document.getElementById("h1").innerHTML = `היום יום : ${day2}`;
 }
 
 
@@ -23,4 +56,10 @@ function numpressd(){
         calculator_value.value = '';
     }
 
+    }
+
+    function reset(){
+        // calculator = '';
+        document.getElementById("calculator").innerHTML = "";
+        calculator_value.value = '';
     }
