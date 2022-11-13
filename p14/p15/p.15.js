a46ae657f8613983d7e98299c7b62a682fdf79d4
sh <sh770h@gmail.com>
2022-11-13 22:11:25 +0200

    ש

--- p14/p15/p.15.js
+++ p14/p15/p.15.js
@@ -4,9 +4,16 @@ function co1(a) {
     document.getElementById("aa1").textContent = " העתקת את: " + document.getSelection();
 }
 
-document.addEventListener("keydown" ,co2)
+// document.addEventListener("keydown" ,co2)
 
-function co2(e) {
-    let tttr = e.code.replace("Key", "");
-    document.getElementById("aa2").textContent = " הקלדת את התו: " + tttr;
+// function co2(e) {
+//     let tttr = e.code.replace("Key", "");
+//     document.getElementById("aa2").textContent = " הקלדת את התו: " + tttr;
+// }
+// זה יציג רק את התו שהוקלד בלי תוספות
+document.addEventListener("keydown" ,co3)
+
+function co3(e) {
+    let tttr2 = e.code.slice(-1);
+    document.getElementById("aa2").textContent = " הקלדת את התו: " + tttr2;
 }
