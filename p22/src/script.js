3032ce8e7b5ce7efab24cf04ba5ce945a48449bb
sh <sh770h@gmail.com>
2022-12-07 01:16:44 +0200

    22

--- p22/src/script.js
+++ p22/src/script.js
@@ -1,25 +1,21 @@
-let json = fetch("../data_base.json")
-.then(response => response.json())
-.then(data => console.log(data));
+let Data_Base;
 
-// console.log(json)
-// const txt = data
-// const obj = JSON.parse(json)
-// document.getElementById("demo").innerHTML = obj.test_user[username[1]];
 
-// document.getElementById("demo").innerHTML = 55;
-// Object Object.username[1] + ", " + Object.password[1]
+let json = fetch('../data_base.json')
+            .then(r => r.json())
+            .then(data => {
+                Data_Base = data;
+            });
 
-// const myObj = {name:"John", age:30, city:"New York"};
-// const myObj1 = data;
-// document.getElementById("demo").innerHTML = myObj.name;
-// document.getElementById("demo").innerHTML = myObj1.test_user;
-// const text = '[ "Ford", "BMW", "Audi", "Fiat" ]';
-// const myArr = JSON.parse(text);
-// document.getElementById("demo").innerHTML = myArr[0];
 
-// const json = '{"result":true, "count":42}';
-// const obj = JSON.parse(json);
-// document.getElementById("demo").innerHTML = Object.testuser[0];
 
-console.log(json)
+
+
+let showUser = () =>{
+    alert("username: " + Data_Base.test_user.username + " password: " + Data_Base.test_user.password);
+}
+
+
+function redirect(link) {
+    location.assign(link);
+}
