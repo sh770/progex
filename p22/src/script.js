e82612fbb799f11d78c7d1637f152ad3e2affde2
sh <sh770h@gmail.com>
2022-12-07 02:02:54 +0200

    22

--- p22/src/script.js
+++ p22/src/script.js
@@ -1,5 +1,6 @@
 let Data_Base;
-
+            // const username1 = loginForm.username.value;
+            // const password1 = loginForm.password.value;
 
 let json = fetch('../data_base.json')
             .then(r => r.json())
@@ -19,3 +20,32 @@ let showUser = () =>{
 function redirect(link) {
     location.assign(link);
 }
+
+
+
+
+
+
+
+
+
+// function showUser (){
+//     alert("username: " + Data_Base.test_user.username + " password: " + Data_Base.test_user.password);
+//     // alert("username: " + loginForm.username.value + " password: " + loginForm.password.value);
+// }
+
+
+
+
+function login(){
+     if (Data_Base.test_user.username == username && Data_Base.test_user.password == password){
+        alert('you not m22e');
+        redirect('index.html')
+     }else {
+        alert('you not me');
+    }
+
+    
+}
+
+
