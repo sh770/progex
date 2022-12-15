743ede186537a6605549c5aed45051165f40c360
sh <sh770h@gmail.com>
2022-12-15 15:55:15 +0200

    25

--- p22/src/script.js
+++ p22/src/script.js
@@ -2,12 +2,12 @@ let Data_Base;
 
 
 let json = fetch('../data_base.json')
-            .then(r => r.json())
-            .then(data => {
-                Data_Base = data;
-            });
+    .then(r => r.json())
+    .then(data => {
+        Data_Base = data;
+    });
 
-let showUser = () =>{
+let showUser = () => {
     alert("username: " + Data_Base.test_user.username + "\n password: " + Data_Base.test_user.password);
 }
 
@@ -17,16 +17,15 @@ function redirect(link) {
 }
 
 function morph() {
-    if(location.pathname == "/progex/p22/src/index.html"){
-        if(sessionStorage.getItem("isLogin") == "true"){
+    if (location.pathname == "/p22/src/index.html") {
+        if (sessionStorage.getItem("isLogin") == "true") {
             document.getElementById("user-name-h").textContent = `ברוך הבא ${sessionStorage.getItem("username")}`;
             document.getElementById("img-div").style.backgroundImage = "url('../static/prty.png')";
-            document.getElementById("log").style.display = "none";
-        }else{
-         document.getElementById("login1").style.display = "none";   
+            // document.getElementById("log").style.display = "none";
+        } else {
+            // document.getElementById("logout").style.display = "none";
         }
-    }else{
-        
+    } else {
         console.log(localStorage.getItem("isLogin"));
         console.log(location.pathname);
     }
@@ -42,19 +41,33 @@ function login() {
         sessionStorage.setItem("username", Data_Base.test_user.username);
         redirect('index.html');
     } else {
-        alert("Wrong username or password");
+        alert("שם או סיסמה שגויים");
         sessionStorage.setItem("isLogin", false);
     }
 }
 
 
 morph();
-    
 
 
 
-function del(){
+
+function del() {
     sessionStorage.clear()
     redirect('index.html')
 }
 
+function btb1() {
+    if (sessionStorage.getItem("isLogin") == "false") {
+        document.getElementById("logout").style.display = "none";
+    }
+    }
+
+function btb2() {
+    if (sessionStorage.getItem("isLogin") == "true") {
+        document.getElementById("log").style.display = "none";
+    }
+    }
+
+    btb1();
+    btb2();
--- p25/script.js
+++ p25/script.js
@@ -1,15 +1,16 @@
 let Data_Base;
 
 
-let json = fetch('data_base_l1.json')
-            .then(r => r.json())
-            .then(data => {
-                Data_Base = data;
-            });
+let json = fetch('data_base.json')
+    .then(r => r.json())
+    .then(data => {
+        Data_Base = data;
+    });
 
-            console.log(data)
+console.log(data)
 
-let showUser = () =>{
+let showUser = () => {
+    alert(88)
     alert("username: " + Data_Base.users.name + "\n password: " + Data_Base.users.password);
 }
 
@@ -17,18 +18,28 @@ let showUser = () =>{
 function redirect(link) {
     window.location.assign(link);
 }
+// function btb() {
+//     if (sessionStorage.getItem("isLogin") == "true") {
+//         document.getElementById("log").style.display = "none";
+//     } else {
+//         document.getElementById("logout").style.display = "none";
+//     }
+// }
+
 
 function morph() {
-    if(location.pathname == "/p25/index.html"){
-        if(sessionStorage.getItem("isLogin") == "true"){
+    alert(888)
+
+    if (location.pathname == "/p25/index.html") {
+
+        if (sessionStorage.getItem("isLogin") == "true") {
             document.getElementById("user-name-h").textContent = `ברוך הבא ${sessionStorage.getItem("username")}`;
             document.getElementById("img-div").style.backgroundImage = "url('../static/prty.png')";
             document.getElementById("log").style.display = "none";
-        }else{
-         document.getElementById("login1").style.display = "none";   
+        } else {
+            document.getElementById("logout").style.display = "none";
         }
-    }else{
-        
+    } else {
         console.log(localStorage.getItem("isLogin"));
         console.log(location.pathname);
     }
@@ -38,25 +49,30 @@ function morph() {
 function login() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
+
     // setTimeout(() => {}, 2000);
     if (username == Data_Base.test_user.username && password == Data_Base.test_user.password) {
+        // alert(55)
+
         sessionStorage.setItem("isLogin", true);
         sessionStorage.setItem("username", Data_Base.test_user.username);
         redirect('index.html');
     } else {
-        alert("Wrong username or password");
+        alert("שם או סיסמה שגויים");
         sessionStorage.setItem("isLogin", false);
     }
+
 }
 
 
-morph();
-    
 
 
 
-function del(){
+
+function del() {
     sessionStorage.clear()
     redirect('index.html')
 }
 
+morph();
+btb();
